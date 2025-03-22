d3f8100f031ebf299c7f4bee784d8e64010fd128
lucas ziro <luscas.guimaraes@gmail.com>
2025-03-22 00:51:21 -0300

    more stuff

--- src/App.js
+++ src/App.js
@@ -30,8 +30,18 @@ function App() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <i className="fa fa-linkedin"></i>
+              <i className="fa fa-linkedin" />
               LinkedIn
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.instagram.com/zzziiro/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fa fa-instagram" />
+              Instagram
             </a>
           </li>
           <li>
@@ -41,7 +51,7 @@ function App() {
               rel="noopener noreferrer"
             >
               Github
-              <i className="fa fa-github"></i>
+              <i className="fa fa-github" />
             </a>
           </li>
         </ul>
@@ -81,7 +91,109 @@ function App() {
         <div className="desc">
           <h1>Development</h1>
         </div>
-        <></>
+        <div
+          style={{
+            textAlign: "center",
+            marginBottom: 20,
+          }}
+        >
+          <i
+            style={{ color: "#9f56db" }}
+            alt="CSS3"
+            className="devicon devicon-css3-plain"
+          />
+          <i
+            style={{ color: "#9f56db" }}
+            alt="Javascript"
+            className="devicon devicon-javascript-plain"
+          />
+          <i
+            style={{ color: "#9f56db" }}
+            alt="HTML5"
+            className="devicon devicon-html5-plain"
+          />
+          <i
+            style={{ color: "#9f56db" }}
+            alt="Typescript"
+            className="devicon devicon-typescript-plain"
+          />
+          <i
+            style={{ color: "#9f56db" }}
+            alt="React"
+            className="devicon devicon-react-original"
+          />
+          <i
+            style={{ color: "#9f56db" }}
+            alt="Tailwind CSS"
+            className="devicon devicon-tailwindcss-original"
+          />
+          <i
+            style={{ color: "#9f56db" }}
+            alt="Git"
+            className="devicon devicon-git-plain"
+          />
+          <i
+            style={{ color: "#9f56db" }}
+            alt="GitHub"
+            className="devicon devicon-github-original"
+          />
+          <i
+            style={{ color: "#9f56db" }}
+            alt="Docker"
+            className="devicon devicon-docker-plain"
+          />
+          <i
+            style={{ color: "#9f56db" }}
+            alt="AntDesign"
+            className="devicon devicon-antdesign-plain"
+          />
+          <i
+            style={{ color: "#9f56db" }}
+            alt="NodeJS"
+            className="devicon devicon-nodejs-plain"
+          />
+          <i
+            style={{ color: "#9f56db" }}
+            alt="PostgreSQL"
+            className="devicon devicon-postgresql-plain"
+          />
+          <i
+            style={{ color: "#9f56db" }}
+            alt="Bash"
+            className="devicon devicon-bash-plain"
+          />
+          <i
+            style={{ color: "#9f56db" }}
+            alt="Vitest"
+            className="devicon devicon-vitest-plain"
+          />
+          <i
+            style={{ color: "#9f56db" }}
+            alt="Linux"
+            className="devicon devicon-linux-plain"
+          />
+        </div>
+        <div class="row">
+          <div class="column">
+            <h2 className="item-title">Pokédex</h2>
+            <p style={{ fontSize: "2.5vh" }}>
+              Aplicação web responsável por listar todos os pokémons e
+              possibilitar visualizar dados específicos de cada um. Para tal, é
+              utilizada a PokéAPI (disponível em https://pokeapi.co/), através
+              de requisições REST que retornarão os dados desejados.
+            </p>
+            <p style={{ fontSize: "2.5vh" }}>
+              A ideia estética do projeto foi inspirada na Pokédex dos jogos
+              clássicos de Pokémon, com um design simples e intuitivo. Busquei
+              trazer a familiaridade dos jogos para o ambiente web, com um
+              layout limpo e organizado, mas prezando pela fidelidade gráfica da
+              época.
+            </p>
+          </div>
+          <div class="column">
+            <img style={{ width: "80vh" }} src="/pokédex.gif" alt="pokedex" />
+          </div>
+        </div>
         <div className="sub-btn code-btn" onClick={() => setShowSecB(true)}>
           See Design
         </div>
