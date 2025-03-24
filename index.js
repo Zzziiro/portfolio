0c249e219195e352c50dd77f9f4ce078e7dba8ee
lucas ziro <luscas.guimaraes@gmail.com>
2025-03-23 22:24:14 -0300

    sera?

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./src/App";
-import reportWebVitals from "./src/reportWebVitals";
+import App from "src/index.js";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
